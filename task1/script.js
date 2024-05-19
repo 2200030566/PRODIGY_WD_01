707316f2090a59b05c99e76126494d395db942cf
YADLAPALLI SIVA NAGA DURGA <116977324+2200030566@users.noreply.github.com>
2024-05-19 19:38:27 +0530

    Delete task1 directory

--- task1/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    var navigation = document.querySelector('.navigation');
-    if (window.scrollY > 0) {
-        navigation.classList.add('scrolled');
-    } else {
-        navigation.classList.remove('scrolled');
-    }
-});
